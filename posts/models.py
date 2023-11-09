9d28ef1368d244a8bf250ac94587f064808bd6d3
unknown <karimt201>
2023-11-09 08:05:23 +0300

    add admin customization

--- posts/models.py
+++ posts/models.py
@@ -24,3 +24,6 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=20000)
     draft = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
