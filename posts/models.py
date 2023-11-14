3e07f05c853e598a531af763a6c1ca6634c5b5ed
unknown <karimt201>
2023-11-14 09:13:26 +0300

    add coment to model

--- posts/models.py
+++ posts/models.py
@@ -23,6 +23,17 @@ from django.contrib.auth.models import User
         - comments
 '''
 
+
+
+
+
+class category(models.Model):
+    name = models.CharField(max_length=100)
+ 
+ 
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     author = models.ForeignKey(User,related_name = 'post_author',on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
@@ -32,8 +43,21 @@ class Post(models.Model):
     # publish_date2 = models.DateTimeField(auto_now=True)
     tags = TaggableManager()
     image = models.ImageField(upload_to='post')
+    category = models.ForeignKey(category,related_name='post_category',on_delete=models.SET_NULL,null=True)
+
 
 
 
     def __str__(self):
         return self.title
+    
+
+class coment(models.Model):
+    post = models.ForeignKey(Post,related_name='coment_post',on_delete=models.CASCADE)
+    user = models.CharField(max_length=100)
+    coment = models.TextField(max_length=300)
+    created_at = models.DateTimeField(default=timezone.now)
+
+
+    def __str__(self):
+        return str(self.post)
