6736f8690fa1220daf076b0c2e44bb4b4a36834c
unknown <karimt201>
2023-11-15 11:07:44 +0300

    add post_list

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+
+def post_list (request):
+    data = Post.objects.all()   #db : all posts --> list [1,2]
+
+    context = {
+        'karim' : data
+    }
+
+
+
+    return render(request,'posts/post_list.html',context)
+
+
+def post_details():
+    pass
--- project/settings.py
+++ project/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'posts',
     "taggit",
-    'django_extensions',
 ]
 
 MIDDLEWARE = [
