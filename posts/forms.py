d98d78e3965d8bb495a8f7e9ad624abb158074f5
unknown <karimt201>
2023-11-23 11:48:25 +0300

    add new comment form , show comments count

--- posts/forms.py
+++ posts/forms.py
@@ -1,9 +1,15 @@
 from django import forms
-from .models import Post
+from .models import Post , coment
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post 
         #fields = '__all__'
         exclude = ('author',)
+
+class CommentForm(forms.ModelForm) :
+    class Meta :
+        model = coment
+        fields = ['user','coment']
+
         
