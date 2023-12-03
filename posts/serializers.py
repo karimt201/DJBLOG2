490b4666cd409a9da508edbb406b86a35d8d90d6
unknown <karimt201>
2023-12-03 19:34:22 +0300

    show relation as nested object

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import Post
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id' , 'username' , 'email']
 
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField()
+    author = UserSerializer()
     category = serializers.StringRelatedField()
     class Meta:
         model = Post 
