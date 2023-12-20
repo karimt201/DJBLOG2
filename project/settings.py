901b03e6d644452f7aaae3c3f2fdc66f99b7f822
karim tarek <142750275+karimt201@users.noreply.github.com>
2023-12-20 20:40:43 +0300

    add profile page

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,8 +141,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-MADIA_URL = "media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT =os.path.join(BASE_DIR,'media')
+MADIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
